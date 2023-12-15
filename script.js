8f5e1ce56a567c8e8210ec4c3586420d71abb4ee
ADRIAN <adrianbahri87@gmail.com>
2023-12-15 18:11:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 // Konversi ke biner
 function konversibiner(input) {
   const biner = input.toString(2).padStart(4, "0");
-
-  for (let i = 0; i < biner.length; i++) {
-    const value = biner[i];
-    const element = document.getElementById(
-      `input${String.fromCharCode(87 + i)}`
-    );
-    if (element) {
-      element.textContent = value;
-    }
+  const tabel_input = document.querySelectorAll(".input");
+  for (let i = 0; i < tabel_input.length; i++) {
+    tabel_input[i].textContent = biner[i];
   }
 }
 
 // Fungsi untuk mendapatkan hasil tabel kebenaran
-function getHasilTabelKebenaran(desimal) {
+function tabel(desimal) {
   const tabel_kebenaran = [
     { A: 1, B: 1, C: 1, D: 1, E: 1, F: 1, G: 0 },
     { A: 0, B: 1, C: 1, D: 0, E: 0, F: 0, G: 0 },
@@ -27,7 +21,7 @@ function getHasilTabelKebenaran(desimal) {
     { A: 1, B: 1, C: 1, D: 1, E: 1, F: 1, G: 1 },
     { A: 1, B: 1, C: 1, D: 1, E: 0, F: 1, G: 1 },
   ];
-  return (hasil = tabel_kebenaran[desimal]);
+  return tabel_kebenaran[desimal];
 }
 
 // Fungsi untuk menampilkan hasil
@@ -37,17 +31,17 @@ function tampilkanHasil(hasil) {
   keys.forEach((key) => {
     const value = hasil[key];
     const input = document.querySelector(`#output${key}`);
-
-    const segment = document.querySelector(`#${key}`);
-    segment.style.backgroundColor = segment.style.borderColor = value === 1 ? "#39ff14" : "";
+    const segment = document.querySelector(`.${key.toLowerCase()}`);
+    segment.style.backgroundColor = value === 1 ? "#39ff14" : "";
+    segment.style.borderColor = value === 1 ? "#39ff14" : "";
     input.style.color = value === 1 ? "#39ff14" : "";
     input.textContent = value;
   });
 }
 
+// fungsi utama
 function inputNumber(number) {
-  const input = number;
-  const hasil = getHasilTabelKebenaran(input);
+  const hasil = tabel(number);
   tampilkanHasil(hasil);
-  konversibiner(input);
+  konversibiner(number);
 }
